50f5f5ab9b7d0e604fbd32da2ef5eadcdd992bb4
Marek Skotny <marekskotny@gmail.com>
2024-09-13 23:31:14 +0200

    Initial commit

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, MatCardModule, MatButtonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
