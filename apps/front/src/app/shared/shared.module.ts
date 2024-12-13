61b95961eb18c00f252062e4f760a80356d1443e
Marek Skotny <marekskotny@gmail.com>
2024-12-13 15:42:43 +0100

    [Feat-8] - Sidebar

--- apps/front/src/app/shared/shared.module.ts
+++ apps/front/src/app/shared/shared.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbar } from '@angular/material/toolbar';
-import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MatList, MatListItem, MatNavList } from '@angular/material/list';
-import { RouterLink } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 
 @NgModule({
@@ -15,15 +13,8 @@ import { MatIcon } from '@angular/material/icon';
     CommonModule,
     MatToolbar,
     NgOptimizedImage,
-    MatSidenavContainer,
-    MatSidenavContent,
-    MatSidenav,
-    MatNavList,
-    MatListItem,
-    RouterLink,
     MatIcon,
-    MatList,
-
+    RouterLink,
   ],
   exports: [
     NavbarComponent,
